d744b1b539b4ef23d6f644818cd60febe02618ca
Bichngocc07 <ngoc01072006@gmail.com>
2026-03-09 23:44:42 +0700

    Remove unnecessary line breaks in BaiTapLambda.java

--- Ss02_Java_Andvanced/src/BaiTapLambda.java
+++ Ss02_Java_Andvanced/src/BaiTapLambda.java
@@ -7,11 +7,10 @@ public class BaiTapLambda {
 
     public static void main(String[] args) {
 
-        // Lambda expression kiểm tra mật khẩu >= 8 ký tự
         PasswordValidator validator = password -> password.length() >= 8;
 
-        // Kiểm tra 2 mật khẩu
         System.out.println("12345678 -> " + validator.isValid("12345678"));
         System.out.println("1234 -> " + validator.isValid("1234"));
     }
+
 }
