f7da926f31c6665bbcdcd4614b288d9218df9ea6
Bichngocc07 <ngoc01072006@gmail.com>
2026-03-09 23:44:09 +0700

    Update BaiTapFunctionalInterface.java

--- Ss02_Java_Andvanced/src/BaiTapFunctionalInterface.java
+++ Ss02_Java_Andvanced/src/BaiTapFunctionalInterface.java
@@ -32,22 +32,21 @@ public class BaiTapFunctionalInterface {
 
         Userr user = new Userr("admin01", "ADMIN");
 
-        // 1. Predicate: kiểm tra User có phải Admin không
+       
         Predicate<Userr> isAdmin = u -> u.getRole().equals("ADMIN");
         System.out.println("Is Admin: " + isAdmin.test(user));
 
-        // 2. Function: chuyển User thành String username
+       
         Function<Userr, String> getUsername = u -> u.getUsername();
         String username = getUsername.apply(user);
         System.out.println("Username: " + username);
 
-        // 3. Consumer: in thông tin User ra console
         Consumer<Userr> printUser = u -> System.out.println("User info: " + u);
         printUser.accept(user);
 
-        // 4. Supplier: khởi tạo User mặc định
         Supplier<Userr> createDefaultUser = () -> new Userr("guest", "USER");
         Userr defaultUser = createDefaultUser.get();
         System.out.println("Default User: " + defaultUser);
     }
+
 }
