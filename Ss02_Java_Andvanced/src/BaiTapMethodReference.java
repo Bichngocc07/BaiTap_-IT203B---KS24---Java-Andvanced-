36c7b7dece14da590d8c68dcf298bb2ad30adaea
Bichngocc07 <ngoc01072006@gmail.com>
2026-03-09 23:45:29 +0700

    Update BaiTapMethodReference.java

--- Ss02_Java_Andvanced/src/BaiTapMethodReference.java
+++ Ss02_Java_Andvanced/src/BaiTapMethodReference.java
@@ -22,32 +22,27 @@ public class BaiTapMethodReference {
 
     public static void main(String[] args) {
 
-        // Danh sách users
         List<User> users = Arrays.asList(
                 new User("Alice"),
                 new User("Bob"),
                 new User("Charlie")
         );
 
-        // 1. Lambda: (user) -> user.getUsername()
-        // Method Reference:
+       
         Function<User, String> getUsername = User::getUsername;
 
-        // 2. Lambda: (s) -> System.out.println(s)
-        // Method Reference:
+       
         Consumer<String> print = System.out::println;
 
-        // 3. Lambda: () -> new User()
-        // Method Reference:
+       
         Supplier<User> createUser = User::new;
 
-        // Áp dụng với danh sách users
         users.stream()
                 .map(getUsername)
                 .forEach(print);
 
-        // Tạo user mới bằng constructor reference
         User newUser = createUser.get();
         System.out.println("New User: " + newUser.getUsername());
     }
+
 }
